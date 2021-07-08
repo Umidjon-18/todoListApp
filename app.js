dd98626520c3b692babc8642f977e95ffa8970d8
Umidjon-18 <umidjonxomidjonovich@gmail.com>
2021-07-08 16:17:33 +0500

    Added a Procfile.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-umidjon:Test123@cluster0.gznld.mongodb.net/todoListDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -131,10 +131,13 @@ app.post("/delete", function(req, res){
 
 });
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
 
-app.listen(3000, function() {
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
